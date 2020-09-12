4e540e0fcc54d9d5458188a6c72cc6adc8c3237f
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-12 20:43:12 +0700

    initializing project

---
+++ src/test/java/com/rg/microservices/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rg.microservices.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
